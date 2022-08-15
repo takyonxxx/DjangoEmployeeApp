f60137e82b593abe3d0da457dbcf10a954a8e7c5
turkay.biliyor <turkay.biliyor@barikat.com.tr>
2022-08-15 13:50:22 +0300

    pass

--- Employee/models.py
+++ Employee/models.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
-# from mirage import fields
+from mirage import fields
 
 
 # Create your models here.
@@ -8,8 +9,8 @@ class Employee(models.Model):
     name = models.CharField(max_length=50, verbose_name="Full Name")
     email = models.EmailField(max_length=99, blank=True, verbose_name="Email")
     username = models.CharField(max_length=255, blank=True, default='')
-    # password = fields.EncryptedCharField(blank=True, default='')
-    password = models.CharField(max_length=99, blank=True, default='')
+    password = fields.EncryptedCharField(blank=True, default='')
+    # password = models.CharField(max_length=99, blank=True, default='')
     is_active = models.BooleanField(default=False, verbose_name="Active")
     created = models.DateTimeField(default=timezone.now)
     REQUIRED_FIELDS = ['name']
@@ -26,3 +27,10 @@ class Employee(models.Model):
         db_table = 'employee'
         ordering = ('-created',)
         # abstract = True
+
+    def save(self, *args, **kwargs):
+        try:
+            super().full_clean()
+            super().save(*args, **kwargs)
+        except ValidationError as err:
+            raise ValidationError(err)
